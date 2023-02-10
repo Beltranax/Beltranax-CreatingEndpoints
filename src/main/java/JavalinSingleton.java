aaf140cb330b30cf996287a9861e99616d551938
Beltranax <beltranangel7293@gmail.com>
2023-02-10 00:01:41 +0000

    Lab Completed

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -16,6 +16,12 @@ public class JavalinSingleton {
      */
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
+
+
+        app.get("/hello", ctx -> {
+            //logic to be executed when this endpoint is hit
+            ctx.result("Hello World");
+        });
         
         //write endpoint here
 
